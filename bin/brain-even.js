b55bc1e09eb2cde01de0679715f0a319e795655c
rogedkone <rogedkone@pm.me>
2022-05-19 04:00:25 +0300

    working prototype even game

---
+++ bin/brain-even.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+import greeting from '../src/cli.js';
+
+console.log('Welcome to the Brain Games!');
+const name = greeting();
+
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+let count = 0;
+while (count !== 3) {
+  const number = Math.floor(Math.random() * 101);
+  console.log(`Question: ${number}`);
+  const answer = readlineSync.question('Your answer: ');
+  const correctAnswer = number % 2 === 0 ? 'yes' : 'no';
+  if (answer !== 'yes' && answer !== 'no') {
+    console.log(`'${answer}' is wrong answer ;C. Correct answer was '${correctAnswer}'`);
+    break;
+  }
+  if (answer === correctAnswer) {
+    console.log('Correct!');
+    count += 1;
+  } else {
+    console.log(`'${answer}' is wrong answer ;C. Correct answer was '${correctAnswer}'`);
+    break;
+  }
+  if (count === 3) {
+    console.log(`Congratulations, ${name}!`);
+  }
+}
