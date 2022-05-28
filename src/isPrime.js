0271865430c7f0e6444f7da57fca0988bafd5e4a
rogedkone <rogedkone@pm.me>
2022-05-28 16:57:47 +0300

    fix isPrime function

--- src/isPrime.js
+++ src/isPrime.js
@@ -1,6 +1,6 @@
 const isPrime = (num) => {
-  for (let i = 2; i < num; i += 1) { if (num % i === 0) return false; }
-  return num > 1 ? 'yes' : 'no';
+  for (let i = 2, s = Math.sqrt(num); i <= s; i += 1) { if (num % i === 0) return 'no'; }
+  return 'yes';
 };
 
 export default isPrime;
